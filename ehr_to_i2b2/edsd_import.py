0a24febf2f35a2e87b712658d400128277976fa6
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-04-26 19:04:25 +0200

    implement import of EDSD EHR data

--- ehr_to_i2b2/edsd_import.py
+++ ehr_to_i2b2/edsd_import.py
@@ -1,5 +1,17 @@
-import logging
+import pandas
+
+from glob import iglob
+from os.path import join
+
+
+EHR_FILENAME = "EDSD-multicenter.csv"
+SEPARATOR = ";"
+
+INTERESTING_COLUMNS = ["PatientID", "Age", "Gender", "Handedness", "Patient'sBirthDate", "Diagnosis", "MMSE_TOT",
+                       "MagneticFieldStrength", "center", "mri_date", "MMSE_date"]
 
 
 def process(input_folder, i2b2_conn, dataset):
-    logging.info("No implementation for this dataset...")
+    ehr_file_path = next(iglob(join(input_folder, '**/' + EHR_FILENAME), recursive=True))
+    df = pandas.read_csv(ehr_file_path, sep=SEPARATOR, low_memory=False)
+    df = df[INTERESTING_COLUMNS]
